9fa610f9032d499a06339adcaefd4e2a801a0cc1
Lina Meyer <linammeyer@gmail.com>
2018-05-21 16:42:13 +0200

    index and show done

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,9 +3,9 @@ class Boat < ApplicationRecord
   has_many :bookings
 
   validates :price, presence:true
-  validates :description, presence:true
+  # validates :description, presence:true
   validates :category, presence:true
   validates :name, presence:true
-  validates :availability, presence:true
-  validates :capacity, presence:true
+  # validates :availability, presence:true
+  # validates :capacity, presence:true
 end
