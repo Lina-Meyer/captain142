39163ab9d919b1ddbbd7356ea03802744338ad70
Dania Maciel <daniaemily@gmail.com>
2018-05-22 14:48:00 +0200

    js map

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -3,7 +3,13 @@ class BoatsController < ApplicationController
   end
 
   def index
-    @boats = Boat.all
+    @boats = Boat.where.not(latitude: nil, longitude: nil)
+    @markers = @boats.map do |boat|
+      {
+        lat: boat.latitude,
+        lng: boat.longitude#,
+      }
+    end
   end
 
   def show
