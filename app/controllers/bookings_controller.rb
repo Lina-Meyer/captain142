db61740333bd05133cbc882c4afedd329790de08
Lina Meyer <linammeyer@gmail.com>
2018-05-23 16:59:13 +0200

    finished bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,28 @@
 class BookingsController < ApplicationController
+  def show
+    @boat = Boat.find(params[:boat_id])
+    authorize @boat
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
   def create
-    raise
+    # initialize new booking with start and end date
+    @booking = Booking.new(booking_params)
+    # assign boat to booking
+    @boat = Boat.find(params[:boat_id])
+    @booking.boat = @boat
+    # assign current user to booking
+    @booking.user = current_user
+    @booking.save
+
+    authorize @booking
+    redirect_to boat_booking_path(@boat, @booking)
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
