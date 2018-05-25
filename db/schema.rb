24cf368e90ac9f8e525cb8ad8991ea1c9820e10a
Lina Meyer <linammeyer@gmail.com>
2018-05-25 16:09:32 +0200

    Revert "schema"
    This reverts commit 9b5021ec0b06e134936a2e94081db86d5e70d7c1.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_094427) do
+ActiveRecord::Schema.define(version: 2018_05_25_101611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2018_05_22_094427) do
     t.integer "price"
     t.text "description"
     t.string "category"
-    t.boolean "availability", default: true
+    t.boolean "availability"
     t.integer "capacity"
     t.string "city"
     t.bigint "user_id"
@@ -35,13 +35,22 @@ ActiveRecord::Schema.define(version: 2018_05_22_094427) do
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.string "status", default: "pending"
+    t.string "status"
     t.bigint "boat_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["boat_id"], name: "index_bookings_on_boat_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "stars"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,4 +74,5 @@ ActiveRecord::Schema.define(version: 2018_05_22_094427) do
   add_foreign_key "boats", "users"
   add_foreign_key "bookings", "boats"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "bookings"
 end
